245e2bad8276b619721598eaf5b83fd55a61caba
Luka <lukaguledani123@gmail.com>
2026-07-01 21:24:21 +0400

    Add project files for upload

---
+++ Player/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 250.0
+const JUMP_VELOCITY = -450.0
+
+func _physics_process(delta):
+	# Add gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Left / Right movement.
+	var direction := Input.get_axis("ui_left", "ui_right")
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
